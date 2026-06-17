3abd15cf93a63ab18134a7a522a1bfb4989dee8f
rus-leb <robberyteam7@gmail.com>
2026-06-17 07:40:22 +0300

    Configured workspace

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000
+const port = 3000;
 
 // midleware
 app.use(bodyParser.json());
 
 app.listen(port, () => {
-    console.log("Server listening on part ${port} and starting at http://localhost:3000");
-})
+    console.log(
+        'Server listening on part ${port} and starting at http://localhost:3000'
+    );
+});
