0d3bf6dc3b8313660efed4472bcccfba06fb82a0
rus-leb <robberyteam7@gmail.com>
2026-06-18 04:01:16 +0300

    fix

--- config/swagger.js
+++ config/swagger.js
@@ -1,38 +1,17 @@
-import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import express from 'express';
 
 const router = express.Router();
 
-const options = {
-definition: {
+const swaggerDocument = {
 openapi: '3.0.0',
-info: {
-title: 'Task Manager API',
-version: '1.0.0',
-description: 'Task Manager API Implementation',
-},
-servers: [
-{
-url: 'http://localhost:3000',
-description: 'Development server',
-},
-],
-components: {
-securitySchemes: {
-ApiKeyAuth: {
-type: 'apiKey',
-in: 'header',
-name: 'user-id',
-},
-},
-},
-},
-apis: ['./routes/*.js'],
+info: { title: 'Task Manager API', version: '1.0.0' },
+paths: {
+'/api/auth/register': { post: { tags: ['Auth'], summary: 'Register' } },
+'/api/auth/login': { post: { tags: ['Auth'], summary: 'Login' } },
+'/api/task': {
+post: { tags: ['Task'], summary: 'Create task' },
+get: { tags: ['Task'], summary: 'Get tasks' }
+}
+}
 };
-
-const specs = swaggerJsdoc(options);
-
-router.use('/docs', swaggerUi.serve, swaggerUi.setup(specs));
-
-export default router;
