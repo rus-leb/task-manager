40303d03ddab7d59d375ab138ca88adbfa02f2ef
rus-leb <robberyteam7@gmail.com>
2026-06-18 05:21:07 +0300

    fix: final

--- config/db.js
+++ config/db.js
@@ -3,11 +3,20 @@ import dns from 'node:dns';
 
 dns.setServers(["1.1.1.1", "1.0.0.1"]);
 
-const URI = 
-'mongodb+srv://robberyteam7_db_user:room12@cluster0.a1uplna.mongodb.net/test?appName=Cluster0';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+const dbUri = "mongodb+srv://robberyteam7_db_user:test111@cluster0.a1uplna.mongodb.net/?appName=Cluster0"
 
 mongoose
-.connect(URI)
+.connect(dbUri)
 .then(() => {
     console.log('Conected to MongoDB');
 })
